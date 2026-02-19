475ce88067e440f9337237c398fa6024ff6165a0
sontosh36 <sontosh159700@gmail.com>
2026-02-19 12:17:44 +0600

    connected to the mongoDB database.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,10 +9,36 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l1sfp1m.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("fundStackDB");
+    const loansCollection = database.collection("loans");
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // don't write
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("FundStack server content ready here!");
+});
 
 app.listen(port, () => {
-  console.log(`FundStack server listening on port ${port}`)
-})
+  console.log(`FundStack server listening on port ${port}`);
+});
