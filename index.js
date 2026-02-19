21977956aa00066a83d6792cf044d7e48869a832
sontosh36 <sontosh159700@gmail.com>
2026-02-19 11:15:31 +0600

    initial configration setup

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`FundStack server listening on port ${port}`)
+})
